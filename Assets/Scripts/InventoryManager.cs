fc989295e76d7dc052b33b0a2e88b309b4f2792b
brendanpayne <payneb2@mail.uc.edu>
2024-02-28 13:23:57 -0500

    stackable items now stack

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -6,12 +6,25 @@ public class InventoryManager : MonoBehaviour
 {
     public InventorySlot[] inventorySlots;
     public GameObject inventoryItemPrefab;
+    public int maxStackAmount = 99;
     public bool AddItem(Item item)
     {
-
-        //TODO: Implement stackable item handling
-
-        Debug.Log("AddItem");
+        // stack check
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            bool isSameItem = itemInSlot != null && itemInSlot.item == item;
+            Debug.Log("isSameItem: " + isSameItem);
+            bool isStackingItem = isSameItem && itemInSlot.item.isStackable && itemInSlot.amount < maxStackAmount;
+            Debug.Log("isStackingItem: " + isStackingItem);
+            if (isStackingItem)
+            {
+                itemInSlot.amount++;
+                itemInSlot.RefreshAmount();
+                return true;
+            }
+        }
         // find empty slots
         for (int i = 0; i < inventorySlots.Length; i++)
         {
