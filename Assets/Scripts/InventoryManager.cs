d66e5de69e215ade2bfca781cc3ea01ff18b411c
brendanpayne <payneb2@mail.uc.edu>
2024-02-29 10:55:15 -0500

    inventory slots now selectable

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -6,7 +6,36 @@ public class InventoryManager : MonoBehaviour
 {
     public InventorySlot[] inventorySlots;
     public GameObject inventoryItemPrefab;
+    public GameObject toolbar;
     public int maxStackAmount = 99;
+
+    int selectedSlot = -1;
+
+    public void Start()
+    {
+        SelectSlot(0); // select first slot by default
+    }
+
+    public void Update() {
+        int toolbarLength = toolbar.transform.childCount;
+        if (Input.inputString.Length > 0) {
+            int index = Input.inputString[0] == '0' ? 9 : Input.inputString[0] - '1'; // 0-9
+            if (index >= 0 && index < toolbarLength) {
+                SelectSlot(index);
+            }
+        }
+    }
+
+    public void SelectSlot(int index)
+    {
+        if (selectedSlot != -1)
+        {
+            inventorySlots[selectedSlot].Deselect();
+        }
+        selectedSlot = index;
+        inventorySlots[selectedSlot].Select();
+    }
+
     public bool AddItem(Item item)
     {
         // stack check
@@ -15,9 +44,7 @@ public class InventoryManager : MonoBehaviour
             InventorySlot slot = inventorySlots[i];
             InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
             bool isSameItem = itemInSlot != null && itemInSlot.item == item;
-            Debug.Log("isSameItem: " + isSameItem);
             bool isStackingItem = isSameItem && itemInSlot.item.isStackable && itemInSlot.amount < maxStackAmount;
-            Debug.Log("isStackingItem: " + isStackingItem);
             if (isStackingItem)
             {
                 itemInSlot.amount++;
--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -2,9 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+    public Sprite deselectedSprite, selectedSprite;
+    private Image image;
+
+    public void Awake() {
+        image = GetComponent<Image>();
+        Deselect();
+    }
+
+    public void Select() {
+        image.sprite = selectedSprite;
+    }
+
+    public void Deselect() {
+        image.sprite = deselectedSprite;
+    }
+
     public void OnDrop(PointerEventData eventData) {
         if (transform.childCount == 0) {
             GameObject dropped = eventData.pointerDrag;
